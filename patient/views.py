544ebc5d7dc2363af35b7e8a6dda92f1523fd577
Misbahul Amin <misbahul.amin2001@gmail.com>
2024-07-20 14:43:18 +0800

    resolved agian

--- patient/views.py
+++ patient/views.py
@@ -41,7 +41,7 @@ class RegistrationApiView(APIView):
 def activate(request, uid64, token):
     try:
         uid = urlsafe_base64_decode(uid64).decode()
-        user = User.defalut_manager.get(pk=uid)
+        user = User._default_manager.get(pk=uid)
     except(User.DoesNotExist):
         user = None
     if user is not None and default_token_generator.check_token(user, token):
